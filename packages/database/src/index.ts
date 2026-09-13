d9aece2d9b0952e915c27aeaf8bcca9124b67f2c
acaybak <acaybak@gmail.com>
2026-09-14 00:21:32 +0300

    fix: declare process type inline for Render build

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
+declare const process: { env: { NODE_ENV?: string } };
+
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
